edfdc82ea131610bef090b902e3a278bf4e7c008
Apple <apple@Air-de-Apple.home>
2022-09-02 23:20:16 +0200

    Some progress

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Dashboard from "./containers/dashboard";
 import Login from "./containers/login";
+import Procedures from "./containers/procedures";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path={"/"} element={<Dashboard />} />
+        <Route path={"/procedures"} element={<Procedures />} />
         <Route path={"/login"} element={<Login />} />
       </Routes>
     </Router>
---
+++ client/src/containers/procedures/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import DashboardLayout from "../../layouts/dashboard-layout";
+
+const Procedures = () => {
+  return <DashboardLayout>Procedures</DashboardLayout>;
+};
+
+export default Procedures;
