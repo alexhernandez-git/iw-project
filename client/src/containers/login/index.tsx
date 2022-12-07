32b3ff99aca24380dafc2f43b4d7861fe8bb433f
Apple <apple@MacBook-Air-de-Apple.local>
2022-12-07 11:34:15 +0100

    faewa

--- client/src/containers/login/index.tsx
+++ client/src/containers/login/index.tsx
@@ -68,7 +68,7 @@ export default function Login() {
             alt="Your Company"
           />
           <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-            Sign in to your account
+            Iniciar sesión en su cuenta
           </h2>
           {/* <p className="mt-2 text-center text-sm text-gray-600">
             Or{" "}
