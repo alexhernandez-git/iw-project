e7dbdbbbf60d90297fa032e8d89a80b848409eff
Apple <apple@MacBook-Air-de-Apple.local>
2022-12-21 10:36:39 +0100

    first commit

--- api/src/app.ts
+++ api/src/app.ts
@@ -14,7 +14,7 @@ var cors = require("cors");
 app.use(morgan("dev"));
 var path = require("path");
 const router = express.Router();
-app.use(cors());
+app.use(cors({ optionsSuccessStatus: 200 }));
 app.use(
   fileUpload({
     limits: { fileSize: 50 * 1024 * 1024 },
