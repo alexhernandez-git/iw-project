4e17888c8f47163281ff8e43b1c78e61d09d44f8
Apple <apple@MacBook-Air-de-Apple.local>
2022-12-22 18:01:15 +0100

    first commit

--- client/src/containers/expedients-types-new/index.tsx
+++ client/src/containers/expedients-types-new/index.tsx
@@ -114,10 +114,12 @@ const ExpedientsTypesNew = () => {
                   options:
                     expedientTypesStatus === SliceState.Success &&
                     expedientTypes
-                      ? expedientTypes.data.map(({ nombre, codigo, _id }) => ({
-                          label: `Nombre: ${nombre} Codigo: ${codigo}`,
-                          id: _id,
-                        }))
+                      ? expedientTypes.data
+                          .filter(({ isAreaFuncional }) => isAreaFuncional)
+                          .map(({ nombre, codigo, _id }) => ({
+                            label: `Nombre: ${nombre} Codigo: ${codigo}`,
+                            id: _id,
+                          }))
                       : [],
                 },
               ].map((item) => ({ ...item, formik })),
