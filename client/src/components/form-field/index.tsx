c7a7c5655ea676807a91e552a2cc6c32e331badd
Apple <apple@MacBook-Air-de-Apple.local>
2022-12-28 18:39:38 +0100

    first commit

--- client/src/components/form-field/index.tsx
+++ client/src/components/form-field/index.tsx
@@ -141,6 +141,7 @@ export const FormFileField = ({ data }: Props) => {
   const {
     disabled,
     nombre,
+    onEditFileField,
     getFieldFiles,
     onEditFile,
     getNotUploadedFieldFiles,
@@ -177,6 +178,7 @@ export const FormFileField = ({ data }: Props) => {
               <input
                 disabled={disabled}
                 onChange={(e) => {
+                  onEditFileField(nombre, e.target.files);
                   onEditFile(nombre, e.target.files?.[0]);
                 }}
                 id={nombre}
