97cf29bc22885881866ef70795beea245d3b8766
Apple <apple@MacBook-Air-de-Apple.local>
2022-12-28 17:07:47 +0100

    eefwaafwe

--- api/src/routes/expedients.ts
+++ api/src/routes/expedients.ts
@@ -13,7 +13,7 @@ var router = express.Router();
 router.post("/", create);
 router.get("/", find);
 router.patch("/:id", updateOne);
-router.patch("/:id/:sectionName/:fieldName", updateFile);
+router.patch("/files/:id/:sectionName/:fieldName", updateFile);
 router.get("/:id", findOne);
 router.delete("/:id", deleteOne);
 
